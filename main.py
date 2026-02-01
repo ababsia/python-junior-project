bf5907d56d19938ba5ecfcebe77cb4eef2bc2078
Manel Ababsia <ababsiamanel310@yahoo.com>
2026-01-31 21:16:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-def hello():
-    print("Hello, GitHub!")
+# React Junior Project
 
-hello()
+A small React project created to practice front-end basics.
+
+## Goals
+- Practice React components
+- Practice UI structure
+- Learn how to organize a simple front-end project
+
+## Tech
+- React
+- JavaScript
+
+## Status
+Work in progress (learning project).
+
+## Author
+Manel Ababsia
