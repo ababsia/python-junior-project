2457b7aad6571d7c18bdee4382aaeebe8317cc73
Manel Ababsia <ababsiamanel310@yahoo.com>
2026-01-31 21:17:29 -0600

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: 20 }}>
+      <h1>React Junior Project</h1>
+      <p>Hello GitHub! This is a small project for learning React.</p>
+    </div>
+  );
+}
